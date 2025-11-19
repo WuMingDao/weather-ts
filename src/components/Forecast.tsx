00d83c9f27751f92cf6862ebae5ba0c0830fad77
wumingdao <wumingdao070@gmail.com>
2025-11-20 00:17:36 +0800

    add forecast mock data

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,4 +1,25 @@
 function Forecast() {
-  return <></>;
+  return (
+    <ul className="list bg-base-100 rounded-box">
+      <li className="list-row mx-auto gap-60">
+        <div className="flex items-center gap-4">
+          <div>
+            <img
+              className="size-10 rounded-box"
+              src="https://img.daisyui.com/images/profile/demo/1@94.webp"
+            />
+          </div>
+          <div>
+            <div>Dio Lupa</div>
+            <div className="text-xs uppercase font-semibold opacity-60">
+              Remaining Reason
+            </div>
+          </div>
+        </div>
+
+        <div className="text-2xl">20 / 30</div>
+      </li>
+    </ul>
+  );
 }
 export default Forecast;
